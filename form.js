54435095856a51ac633cba5140647049f62f5b51
dkomarovo <dkomarov@hawk.iit.edu>
2017-10-27 21:43:07 -0500

    New onclick & mouse movement functions

--- form.js
+++ form.js
@@ -1,32 +1,23 @@
-function confirmation() {
+function formValidator() {
 
-	if ((document.getElementById('dob_field').value == "") && (document.getElementById('email_field').value == ""))
-	{
-		alert("The 'date of birth' and 'email address' fields are mandatory. Please make sure you have filled 
-		out the 'date of birth' and 'email address' fields.");
-		return false;
+	var x = document.forms["my.Form"]["fName_field"].value;
+	var y = document.forms["my.Form"]["email_field"].value;
+	
+	if ((x == "") || (y == "")) {
+		alert("Incomplete field(s). Optional fields are: last name, phone number. You must provide all other information to continue.");
 	}
-	else 
-	{
-		alert("Form submitted successfully! The fields will now reset.");
-		document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
-			
-	}
-
-	if ((document.getElementById('fName_field').value == "") && (document.getElementById('lName_field').value == "") 
-	&& (document.getElementById('phoneNum_field').value == ""))
-  {
-  	alert("There were empty fields! However, some fields are optional (both first & last name and 
-  	'phone number'). If you do not wish to provide optional information, click 'Submit' once more.");
-		return false;
-	}
-	else 
-	{
-  	alert("Form submitted successfully! The fields will now reset.");
-		document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
-	}
+	else {
+  	alert("Thank you! Have a nice day!");
+		var response = "Thank you!";
+		document.getElementById("results").innerHTML = response;
+}
 
 }
 
+function mouseOver() {
+	document.getElementById("subButton").style.color = "yellow";
+}
 
-
+function mouseOut() {
+	document.getElementById("subButton").style.color = "navy";
+}
